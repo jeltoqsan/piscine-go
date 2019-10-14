23ef0fe622bb71b47b5ad64b82db9c77aa8ae713
jeltoqsan <toksanbayev@gmail.com>
2019-10-14 23:22:00 +0600

    jqID

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg HERO_ID "$HERO_ID" '.[] |select (.id == ($HERO_ID|tonumber)) |.connetions.relatives' | sed 's/"//g'
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg ID "$HERO_ID" '.[] |select (.id == ($ID|tonumber)) |.connetions.relatives' | sed 's/"//g'
