e1f2f56c0d28a8f22f0a8c25d30c1502f7533dac
jeltoqsan <toksanbayev@gmail.com>
2019-10-14 23:39:58 +0600

    nextcommit

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg ID "$HERO_ID" '.[] |select (.id == ($ID|tonumber)) |.connetions.relatives' | sed 's/"//g'
+curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq ".[] | select (.id==$HERO_ID)" | grep 'relatives' | cut -d'"' -f4
