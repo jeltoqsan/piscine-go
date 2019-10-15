d7c1542c4aa549e9965ac6dcbafbbace134bf548
jeltoqsan <toksanbayev@gmail.com>
2019-10-15 20:00:49 +0600

    proverka

--- countfiles.sh
+++ countfiles.sh
@@ -1,2 +1,2 @@
-find $PWD \( -type f -or -type d \) -name ".*" -prune -o | wc -l # type f,d - поиск файлов и директорий
+find $PWD \( -type f -or -type d \) -name ".*" -prune -o -print | wc -l # type f,d - поиск файлов и директорий
 # способ 2 find . | wc -l
