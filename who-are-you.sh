0ae1afdcdccbe118d34d7093780da3521d12f434
jeltoqsan <toksanbayev@gmail.com>
2019-10-15 11:35:21 +0600

    addcomments

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,1 +1,8 @@
-echo '"Batman"'
+curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq '.[51].name' 
+
+# curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq '.[].name' - выводит весь список имен
+# curl - передает данные от указанной ссылки; 
+# 51 - имя бэтман по счету 51ый указан, 
+# -s - убирает/скрывает лишние данные, выведенные на экран.
+# jq - разбор/поиск строк и обращение к нужному полю/параметру
+
