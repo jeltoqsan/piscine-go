c8122c7caa1c982ea48090c8687e2a4db3e43a27
jeltoqsan <toksanbayev@gmail.com>
2019-10-15 19:54:08 +0600

    proverka

--- countfiles.sh
+++ countfiles.sh
@@ -1,1 +1,3 @@
-find -type f , -type d | wc -l
+find . \( -type f , -type d \) | wc -l
+# способ 2 find $PWD \( -type f -or -type d \) -name ".*" -prune -o -print | wc -l
+# способ 3 ilifind . | wc -l
