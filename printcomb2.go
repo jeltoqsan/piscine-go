ee08d3f2f52ea3c184e72d5742e854b80ac73eb1
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 22:49:58 +0600

    change

--- printcomb2.go
+++ printcomb2.go
@@ -6,8 +6,9 @@ func PrintComb2() {
 
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
+			d := b + 1
 			for c := a; c <= '9'; c++ {
-				for d := b + 1; d <= '9'; d++ {
+				for ; d <= '9'; d++ {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(' ')
