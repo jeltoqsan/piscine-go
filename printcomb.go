46aa006c0c0aab48fa0f21fbd2c04e9604203024
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 19:21:10 +0600

    commit1

---
+++ printcomb.go
@@ -0,0 +1,19 @@
+package main
+
+func PrintComb(){
+    for t := '0'; t<='9'; t = t + 1{
+        for o := t+1; o<='9'; o = o + 1{
+            for p := o+1; p<='9';
+			z01.PrintRune(t){
+				z01.PrintRune(o)
+				z01.PrintRune(p)
+		
+				
+					if t<55 {
+						z01.PrintRune(44)
+						z01.PrintRune(32) 
+				}
+			}           
+            }
+z01.PrintRune('\n')
+	} 
