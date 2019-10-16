590ba784ce7761bb9a202d27c73b1063494d2d7f
Yerbol Toksanbayev <52797344+jeltoqsan@users.noreply.github.com>
2019-10-16 12:05:41 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for k := 'a'; k <= 'z'; k = i + 1 {
-		z01.PrintRune(k)
-	}
-	z01.PrintRune('\n')
+    for i := 'a'; i <= 'z'; i++ {
+        z01.PrintRune(i)
+    }
+    z01.PrintRune('\n')
 }
