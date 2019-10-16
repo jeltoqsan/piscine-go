f0e76d58d6a5d463dce633ab80e849a2b870bde9
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 11:37:28 +0600

    nextformatting

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for latin := 'a'; latin <='z'; latin = latin + 1 {
+	for latin := 'a'; latin <= 'z'; latin = latin++ {
 		z01.PrintRune(latin)
 	}
 	z01.PrintRune('\n')
