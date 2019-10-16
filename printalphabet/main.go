7141de8ecc77a5815a0048bc61a22c043d238174
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 09:00:13 +0600

    mewline2

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
 func main () { 
-	fmt.Println ("abcdefghijklmnopqrstuvwxyz\n") // \n - newline - перевод на новую строку
+	fmt.Println ("abcdefghijklmnopqrstuvwxyz") // \n - newline - перевод на новую строку
+	fmt.Println ("\n")
 }
