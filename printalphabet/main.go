d426f7e6cb9a6b9da08483f8c5f02ed5c1269820
Yerbol Toksanbayev <52797344+jeltoqsan@users.noreply.github.com>
2019-10-16 12:01:34 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 'a'; i <= 'z'; i++ {
-		z01.PrintRune(i)
+	for k := 'a'; k <= 'z'; k=i+1 { // i++; если 'а' условно меньше или равно 10, то выводится букву 'b' и дойдя до z = z -цикл останавливается
+		z01.PrintRune(k)
 	}
 	z01.PrintRune('\n')
 }
