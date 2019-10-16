fb883b6f6583b4d6d2c95c274d82a427347d3610
Yerbol Toksanbayev <52797344+jeltoqsan@users.noreply.github.com>
2019-10-16 11:51:13 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,12 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for latin := 'a'; latin <= 'z'; latin++ {
-		z01.PrintRune(latin)
+	for i := 'a'; i <= 'z'; i++ {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 } 
+
+// i = i + 1 - можно и так записать 	
+// i - переменная, ее значение 'a', если 'а' условно меньше или равно 10, то выводится букву 'b' и дойдя до z равно z -цикл останавливается и срабатывает PrintRune; \n - newline - перевод на новую строку
+// 2 способ: func main () { //	fmt.Println ("abcdefghijklmnopqrstuvwxyz") } 
