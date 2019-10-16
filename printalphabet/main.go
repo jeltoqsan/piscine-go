d8812614921a5d31c2c52c3bd9d57b216c202cb0
Yerbol Toksanbayev <52797344+jeltoqsan@users.noreply.github.com>
2019-10-16 11:57:28 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -7,8 +7,4 @@ func main() {
 		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
-} 
-
-// i = i + 1 - можно и так записать 	
-// i - переменная, ее значение 'a', если 'а' условно меньше или равно 10, то выводится букву 'b' и дойдя до z равно z -цикл останавливается и срабатывает PrintRune; \n - newline - перевод на новую строку
-// 2 способ: func main () { //	fmt.Println ("abcdefghijklmnopqrstuvwxyz") } 
+}
