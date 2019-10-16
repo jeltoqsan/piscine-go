fe058fbe579475f2e057de75966c13f0dd4b1f5c
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 19:59:56 +0600

    nextcommit

--- printcomb.go
+++ printcomb.go
@@ -1,19 +1,21 @@
-package main
+package printcomb
 
-func PrintComb(){
-    for t := '0'; t<='9'; t = t + 1{
-        for o := t+1; o<='9'; o = o + 1{
-            for p := o+1; p<='9';
-			z01.PrintRune(t){
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+
+    for t := '0'; t <= '9'; t = t + 1 {
+        for o := t+1; o <= '9'; o = o + 1{
+            for p := o+1; p <= '9'; p++ {
+				z01.PrintRune(t)
 				z01.PrintRune(o)
 				z01.PrintRune(p)
-		
-				
-					if t<55 {
+				if t < 55 {
 						z01.PrintRune(44)
 						z01.PrintRune(32) 
-				}
-			}           
-            }
+					}
+				}           
+			}
+		}
 z01.PrintRune('\n')
-	} 
+	}
