dedc207ea597bc8487ed4d07105b865009c30917
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 11:35:29 +0600

    nextformatting

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -8,8 +8,3 @@ func main() {
 	}
 	z01.PrintRune('\n')
 }
-
-// latin++ - можно и так записать вместо latin + 1
-// latin - переменная, ее значение 'a', если 'а' условно меньше или равно 10, то выводится букву 'b' и дойдя до z равно z -цикл останавливается и срабатывает PrintRune; \n - newline - перевод на новую строку
-// 2 способ: func main () {
-// fmt.Println ("abcdefghijklmnopqrstuvwxyz") }
