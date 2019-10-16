205904549fa3e0f779e4c2947f44e4fb04f62650
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 11:31:19 +0600

    nextformatting

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,13 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for latin := 'a'; latin<='z'; latin = latin + 1 { 
+	for latin := 'a'; latin <='z'; latin = latin + 1 { 
 		z01.PrintRune(latin) 
 	}
 	z01.PrintRune('\n') 
 } 
+
+// latin++ - можно и так записать вместо latin + 1
+// latin - переменная, ее значение 'a', если 'а' условно меньше или равно 10, то выводится букву 'b' и дойдя до z равно z -цикл останавливается и срабатывает PrintRune; \n - newline - перевод на новую строку
+// 2 способ: func main () { 
+// fmt.Println ("abcdefghijklmnopqrstuvwxyz") } 
