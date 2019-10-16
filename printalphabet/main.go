3c46e94d120cddfa3dd4fdf454f92971ddf50ac0
jeltoqsan <toksanbayev@gmail.com>
2019-10-16 15:38:56 +0600

    commit2

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,9 +2,9 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main () {
+func main() {
 	var a rune = 97
-	for i :=a; i <=122; i++ {
+	for i := a; i <= 122; i++ {
 		z01.PrintRune(i)
 	}
 	z01.PrintRune(10)
