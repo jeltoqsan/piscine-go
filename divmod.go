f9558deb6f6167f154f7ef1feaf5c19d9afaeb5d
jeltoqsan <toksanbayev@gmail.com>
2019-10-17 17:53:18 +0600

    divmod

--- divmod.go
+++ divmod.go
@@ -1,9 +1,9 @@
 package piscine
 
-func DivMod(a.b int, div,mod *int) {
-  
-   с := int(a/b) // можно просто a/b
-   d := a%b 
- *div = c  
- *mod = d 
-      }
+func DivMod(a, b int, div, mod *int) {
+
+	с := int(a / b) // можно просто a/b
+	d := a % b
+	*div = c
+	*mod = d
+}
