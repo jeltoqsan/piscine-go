373497dc75b52d97c44b3247ad1be58196865da4
Yerbol Toksanbayev <52797344+jeltoqsan@users.noreply.github.com>
2019-10-17 15:55:25 +0600

    Update ultimatepointone.go

--- ultimatepointone.go
+++ ultimatepointone.go
@@ -1,18 +1,5 @@
 package piscine
 
-import (
-	"fmt"
-
-	piscine ".."
-)
-
 func PointOne(n ***int) {
 	***n = 1
-}
-func main() {
-	a := 1
-	b := &a
-	n := &b
-	piscine.UltimatePointOne(&n)
-	fmt.Println(a)
 }
