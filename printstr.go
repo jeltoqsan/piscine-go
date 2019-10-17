eb935ba28490fba00d23a4c43d919587f961744c
jeltoqsan <toksanbayev@gmail.com>
2019-10-17 21:06:38 +0600

    printstr

--- printstr.go
+++ printstr.go
@@ -1,9 +1,9 @@
 package piscine
 
-imp "github.com/01-edu/z01"
+import "github.com/01-edu/z01"
 
 func PrintRune(str string) {
 	for _, x := range str {
-		z01.PrintRune(string(x))
+		z01.PrintRune(x)
 	}
 }
