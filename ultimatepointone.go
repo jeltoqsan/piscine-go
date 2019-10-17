87307839979ef8b8af93ce12fa9c4a461f8b9e76
jeltoqsan <toksanbayev@gmail.com>
2019-10-17 15:47:48 +0600

    ultimate

---
+++ ultimatepointone.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	piscine ".."
+)
+
+func PointOne(n ***int) {
+	***n = 1
+}
+func main() {
+	a := 1
+	b := &a
+	n := &b
+	piscine.UltimatePointOne(&n)
+	fmt.Println(a)
+}
