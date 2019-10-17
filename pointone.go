491542546bf8baedb25e802fbbcd7f43979c5de0
jeltoqsan <toksanbayev@gmail.com>
2019-10-17 15:12:47 +0600

    pointone

--- pointone.go
+++ pointone.go
@@ -1,10 +1,7 @@
 package piscine
 
-import "fmt"
-
 func PointOne(n *int) { // в функции PointOne n - указывает на int
-	*n = 1
-
+	*n = *n + 1
 }
 
 //Используя указатель (*int) в функции PointOne, мы изменяем значение оригинальной переменной.
