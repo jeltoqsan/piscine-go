617e1a6d3b8d311f85288854cf7274f29d41f455
jeltoqsan <toksanbayev@gmail.com>
2019-10-18 20:24:51 +0600

    strrev.go

--- strrev.go
+++ strrev.go
@@ -1,9 +1,8 @@
 package piscine
 
 func StrRev(s string) string {
-	symb := []rune(s)
-	for i, j := 0, len(symb)-1; i < j; i, j = i+1, j-1 {
-		symb[i], symb[j] = symb[j], symb[i]
-		return string(symb)
+	var x string 
+	for _, word := range {
+		x = string(word) + x
 	}
-}
+	return x
