547f330c1c930af6722b13e01ffc2a82bd6a968e
jeltoqsan <toksanbayev@gmail.com>
2019-10-20 00:25:38 +0600

    update

--- strrev.go
+++
@@ -1,9 +0,0 @@
-package piscine
-
-func StrRev(s string) string {
-	var x string
-	for _, word := range s {
-		x = string(word) + x
-	}
-	return x
-}
--- swap.go
+++
@@ -1,8 +0,0 @@
-package piscine
-
-func Swap(a, b *int) {
-	var x int
-	x = *a
-	*a = *b
-	*b = x
-}
