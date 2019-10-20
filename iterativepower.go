f15a57804e68ec4e6ebe252410549df3c6326c0f
jeltoqsan <toksanbayev@gmail.com>
2019-10-21 03:33:28 +0600

    iterativepower

---
+++ iterativepower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+qwerty:= 1
+if power>0 {
+	return 0
+	}esle{
+		for i:=0; i<power; i++{
+			qwerty *= nb
+		}
+	return qwerty
+	}
+}
