7e6a216fa1ca4817bdad71b11e5780e358ef5bbb
jeltoqsan <toksanbayev@gmail.com>
2019-10-21 18:50:46 +0600

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -1,8 +1,14 @@
 package piscine
 
 func Fibonacci(index int) int {
-	if index <= 1 {
-		return index
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+	if index < 0 {
+		return -1
 	}
 	return Fibonacci(index-1) + Fibonacci(index-2)
 }
