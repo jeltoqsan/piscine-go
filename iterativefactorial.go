66aeb79a06dfe364558c109fc87883952a6a71fb
jeltoqsan <toksanbayev@gmail.com>
2019-10-21 13:30:48 +0600

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,7 +4,7 @@ func IterativeFactorial(nb int) int {
 	if nb >= 0 && nb <= 20 {
 		result := 1
 		for i := 1; i <= nb; i++ {
-			result = result * i
+			result = result * i // result *= i
 		}
 		return result
 	} else {
