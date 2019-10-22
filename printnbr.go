a27707bfdbc892e2113d9cd0ba9db85dcaaf5c2d
jeltoqsan <toksanbayev@gmail.com>
2019-10-22 16:21:46 +0600

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -2,28 +2,29 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintRune(n int) {
+func PrintNb(nb int) {
+	i := '0'
+	if nb == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	for j := 1; j <= nb%10; j++ {
+		i++
+	}
+	for j := -1; j >= nb%10; j-- {
+		i++
+	}
+	if nb/10 != 0 {
+		PrintNb(nb / 10)
+	}
+	z01.PrintRune(i)
+	return
+}
+
+func PrintNbr(n int) {
 
 	if n < 0 {
 		z01.PrintRune('-')
 	}
-	PrintRune(n)
-}
-func PrintNbr(n int) {
-	i := '0'
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-	for j := 1; j <= n%10; j++ {
-		i++
-	}
-	for j := -1; j >= n%10; j-- {
-		i++
-	}
-	if n/10 != 0 {
-		PrintRune(n / 10)
-	}
-	z01.PrintRune(i)
-	return
+	PrintNb(n)
 }
