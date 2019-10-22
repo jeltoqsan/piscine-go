1aa8449c93f41fc097dbe1b417fd341fb98b4290
jeltoqsan <toksanbayev@gmail.com>
2019-10-23 01:10:31 +0600

    nrune

--- nrune.go
+++ nrune.go
@@ -1,8 +1,8 @@
-package main
+//package main
 
-//package piscine
+package piscine
 
-import "github.com/01-edu/z01"
+//import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
 	x := 0
@@ -15,9 +15,9 @@ func NRune(s string, n int) rune {
 	return 0
 }
 
-func main() {
+/*func main() {
 	z01.PrintRune(NRune("Hello!", 3))
 	z01.PrintRune(NRune("Salut!", 2))
 	z01.PrintRune(NRune("Ola!", 4))
 	z01.PrintRune('\n')
-}
+}*/
