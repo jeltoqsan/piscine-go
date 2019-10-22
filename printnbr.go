57673aa909683e9159c7bb6137827fc02f49db8a
jeltoqsan <toksanbayev@gmail.com>
2019-10-22 16:12:07 +0600

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -1,5 +1,31 @@
 package piscine
 
-func PrintRune(n) {
-	fmt.PrintRune("%d", n)
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	var i int //i := '0'
+	i = 0
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	for j := 1; j <= n%10; j++ {
+		i++
+	}
+	for j := -1; j >= n%10; j-- {
+		i++
+	}
+	if n/10 != 0 {
+		PrintNbr(n / 10)
+	}
+	z01.PrintRune(i)
+	return
+}
+
+func PrintNbr(n int) {
+
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	PrintNum(n)
 }
