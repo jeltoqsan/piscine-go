d8da8aa11613289f56fb6ba70fa53457a95269ec
jeltoqsan <toksanbayev@gmail.com>
2019-10-23 00:28:21 +0600

    firstrune

---
+++ firstrune.go
@@ -0,0 +1,17 @@
+//package piscine
+package main
+
+func FirstRune(s string) rune {
+	var a rune
+	for _, z := range s {
+		return z
+	}
+	return a
+}
+
+/*func main() {
+	z01.PrintRune(FirstRune("Hello!"))
+	z01.PrintRune(FirstRune("Salut!"))
+	z01.PrintRune(FirstRune("Ola!"))
+	z01.PrintRune(10)
+}*/
