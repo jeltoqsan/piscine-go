26068e9576b9b38c6e52577dc0698adba25ac7a4
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 03:50:13 +0600

    basicjoin

--- basicjoin.go
+++ basicjoin.go
@@ -1,15 +1,17 @@
 package piscine
+
 /*package main
 
 import "fmt"*/
 
 func BasicJoin(strs []string) string {
 	nb := ""
-for_, r := range strs {
-	nb += r
+	for _, r := range strs {
+		nb += r
+	}
+	return nb
 }
-return nb	
-   }
+
 /*func main() {
 	toConcat := []string{"Hello!", " How", " are", " you?"}
 	fmt.Println(BasicJoin(toConcat))
