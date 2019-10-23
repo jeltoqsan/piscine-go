4b2a8639b3d4dfef7f68dac849285563efcd084b
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 03:47:54 +0600

    basicjoin

---
+++ basicjoin.go
@@ -0,0 +1,16 @@
+package piscine
+/*package main
+
+import "fmt"*/
+
+func BasicJoin(strs []string) string {
+	nb := ""
+for_, r := range strs {
+	nb += r
+}
+return nb	
+   }
+/*func main() {
+	toConcat := []string{"Hello!", " How", " are", " you?"}
+	fmt.Println(BasicJoin(toConcat))
+}*/
