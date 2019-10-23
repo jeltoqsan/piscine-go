0ab6d0b2ae299a217a2fc523c491e934fff0c7a1
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 04:12:01 +0600

    printparams

---
+++ printparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for i, s := range os.Args {
+		if i != 0 {
+			for _, r := range s {
+				z01.PrintRune(r)
+			}
+			z01.PrintRune(10)
+		}
+	}
+}
