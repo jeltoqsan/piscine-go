460c0cd9f80e8352aeea3fead07b8a2b5d2b9625
jeltoqsan <toksanbayev@gmail.com>
2019-10-23 16:58:27 +0600

    tolower

--- tolower.go
+++ tolower.go
@@ -1,12 +1,20 @@
+/*package main
+
+import "fmt"*/
+
 package piscine
 
-func Tolower(s string) string {
+func ToLower(s string) string {
 
 	str := []rune(s)
 	for i, j := range str {
-		if j >= 'Z' && j <= 'A' {
+		if j >= 'A' && j <= 'Z' {
 			str[i] = str[i] + 32
 		}
 	}
 	return string(str)
 }
+
+/*func main() {
+	fmt.Println(ToLower("Hello! How are you?"))
+}*/
