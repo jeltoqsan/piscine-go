4516ead06afda3de7c09b7b1a113993d31ba3dc6
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 02:11:54 +0600

    isnumeric

---
+++ isnumeric.go
@@ -0,0 +1,20 @@
+//package piscine
+
+package main
+
+import "fmt"
+
+func IsNumeric(str string) bool { 
+	for _, r := range str { 
+		if r < '0' || r > '9' { 
+			return false 
+		} 
+	} return true
+}
+
+func main() {
+	fmt.Println(IsNumeric("010203"))
+	fmt.Println(IsNumeric("01,02,03"))
+}
+
+
