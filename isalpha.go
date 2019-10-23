b52bae311a9a8967657d46f3b49fb99b690977af
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 02:02:16 +0600

    isalpha

---
+++ isalpha.go
@@ -0,0 +1,21 @@
+/*package main
+
+import "fmt"*/
+
+//package piscine
+
+func IsAlpha(str string) bool {
+	for _, r := range str {
+		if !((r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9')) {
+			return false
+		}
+	}
+	return true
+}
+
+/*func main() {
+	fmt.Println(IsAlpha("Hello! How are you?"))
+	fmt.Println(IsAlpha("HelloHowareyou"))
+	fmt.Println(IsAlpha("What's this 4?"))
+	fmt.Println(IsAlpha("Whatsthis4"))
+}*/
