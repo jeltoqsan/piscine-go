8f18f04006234dfe077aa182bbfec38b8c60adea
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 18:18:33 +0600

    raid-01

---
+++ raid-01/raid1b.go
@@ -0,0 +1,26 @@
+package Raid1b
+
+import "github.com/01-edu/z01"
+
+func Raid1b(x, y int) {
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+			if i == 1 && j == 1 {
+				z01.PrintRune('/')
+			} else if i == 1 && j == x {
+				z01.PrintRune(92)
+			} else if i == y && j == 1 {
+				z01.PrintRune(92)
+			} else if i == y && j == x {
+				z01.PrintRune('/')
+			} else if i == 1 && j > 1 && j < x || i == y && j > 1 && j < x || i > 1 && i < y && j == 1 || i > 1 && i < y && j == x {
+				z01.PrintRune('*')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		if x > 0 && y > 0 {
+			z01.PrintRune(10)
+		}
+	}
+}
