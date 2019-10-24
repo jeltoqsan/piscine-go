41392196b7591d5b74ce048749da45180d38fe3d
jeltoqsan <toksanbayev@gmail.com>
2019-10-24 17:46:49 +0600

    revparams

--- revparams/main.go
+++ revparams/main.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
-	for i, s := range os.Args {
-		if i = 0 {
-			for _, r := range s {
-				z01.PrintRune(r)
-			}
-			z01.PrintRune(10)
+	counter := 0
+	for i := range os.Args {
+		counter = i
+	}
+	for i := counter; i > 0; i = i - 1 {
+		for _, r := range os.Args[i] {
+			z01.PrintRune(r)
 		}
+		z01.PrintRune(10)
 	}
 }
