abe14daf577df03190b80da57dd8227e54a480e7
jeltoqsan <toksanbayev@gmail.com>
2019-11-06 19:15:54 +0600

    btreeapplyinorder

---
+++ QUEST12/btreeapplyinorder.go
@@ -0,0 +1,46 @@
+package piscine
+
+/*package main
+
+import "fmt"
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if root.Data > data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+	}
+	if root.Left != nil {
+		root.Left.Parent = root
+	}
+	if root.Right != nil {
+		root.Right.Parent = root
+	}
+	return root
+}*/
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	BTreeApplyInorder(root.Left, f)
+	f(root.Data)
+	BTreeApplyInorder(root.Right, f)
+}
+
+/*func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	BTreeApplyInorder(root, fmt.Println)
+
+}*/
