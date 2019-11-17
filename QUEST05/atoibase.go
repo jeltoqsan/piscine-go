8846f4cc173a6cbcfd06d1a3481bb1c255b8534a
jeltoqsan <toksanbayev@gmail.com>
2019-11-17 18:57:04 +0600

    atoibase

--- QUEST05/atoibase.go
+++ QUEST05/atoibase.go
@@ -58,3 +58,32 @@ func main() {
 	fmt.Println(AtoiBase("uoi", "choumi"))
 	fmt.Println(AtoiBase("bbbbbab", "-ab"))
 }
+
+/*
+func AtoiBase(s string, t string) int {
+	ans := 0
+	ln := 0
+	st := map[rune]bool{}
+	for _, c := range t {
+		if st[c] || c == '-' || c == '+' {
+			return ans
+		}
+		st[c] = true
+		ln++
+	}
+	if ln > 1 {
+		for _, c := range s {
+			cnt := 0
+			if st[c] {
+				for _, j := range t {
+					if j == c {
+						break
+					}
+					cnt++
+				}
+				ans = ans*ln + cnt
+			}
+		}
+	}
+	return ans
+}*/
